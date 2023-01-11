bd31c0cb5081fabd4c4372ee4f8689e10aada15a
石博文 <sbw@sbw.so>
2023-01-11 23:56:16 +0800

    use tablegen

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Target/TargetMachine.h"
+#include "llvm/CodeGen/TargetOpcodes.h"
+
+//#include "X86.inc"
 
 using namespace llvm;
 using namespace std;
@@ -22,49 +25,44 @@ int main()
   InitializeAllTargetMCs();
   InitializeAllTargetMCAs();
 
-    // Print registered targets
-    for (auto target : TargetRegistry::targets())
-    {
-        cout << target.getName() << ',' << target.getBackendName() << ',' << target.getShortDescription() << endl;
-    }
+  // Print registered targets
+  for (auto target : TargetRegistry::targets()) {
+      cout << target.getName() << ',' << target.getBackendName() << ',' << target.getShortDescription() << endl;
+  }
 
-    string triple("x86_64-unknown-linux-gnu");
-    string error;
-    const Target *target = TargetRegistry::lookupTarget(triple, error);
-    if (target == nullptr)
-    {
-        cout << error;
-        return -1;
-    }
+  string triple("x86_64-unknown-linux-gnu");
+  string error;
+  const Target *target = TargetRegistry::lookupTarget(triple, error);
+  if (target == nullptr) {
+      cout << error;
+      return -1;
+  }
 
-    cout << "Use Target: " << target->getName() << ',' << target->getBackendName() << ',' << target->getShortDescription() << endl;
+  cout << "Use Target: " << target->getName() << ',' << target->getBackendName() << ',' << target->getShortDescription() << endl;
 
+  MCTargetOptions mcTargetOpt;
+  auto *subTargetInfo = target->createMCSubtargetInfo(triple, "", "");
+  auto *instrInfo = target->createMCInstrInfo();
+  auto *regInfo = target->createMCRegInfo(triple);
+  auto *mcAsmInfo = target->createMCAsmInfo(*regInfo, triple, mcTargetOpt);
+  auto *mcCtx = new MCContext(Triple(triple), mcAsmInfo, regInfo, subTargetInfo);
 
-    MCTargetOptions mcTargetOpt;
-    auto *subTargetInfo = target->createMCSubtargetInfo(triple, "", "");
-    auto *instrInfo = target->createMCInstrInfo();
-    auto *regInfo = target->createMCRegInfo(triple);
-    auto *mcAsmInfo = target->createMCAsmInfo(*regInfo, triple, mcTargetOpt);
-    auto *mcCtx = new MCContext(Triple(triple), mcAsmInfo, regInfo, subTargetInfo);
+  auto *mcCodeEmitter = target->createMCCodeEmitter(*instrInfo, *mcCtx);
 
-    auto *mcCodeEmitter = target->createMCCodeEmitter(*instrInfo, *mcCtx);
+  MCInst inst = MCInstBuilder(TargetOpcode::G_ADD).addImm(1205);
 
-    MCInst inst = MCInstBuilder(2440).addImm(1205);
+  SmallVector<MCFixup, 0> fixups;
+  auto buf = SmallVector<char, 1024>();
+  raw_svector_ostream os(buf);
+  mcCodeEmitter->encodeInstruction(inst, os, fixups, *subTargetInfo);
 
-    SmallVector<MCFixup, 0> fixups;
-    auto buf = SmallVector<char, 1024>();
-    raw_svector_ostream os(buf);
-    mcCodeEmitter->encodeInstruction(inst, os, fixups, *subTargetInfo);
+  for (auto c : buf) {
+    llvm::outs() << format_hex((unsigned char)c, 4) << ' ';
+  }
 
-    auto *asmPrinter = target->createMCInstPrinter(Triple(triple), 1, *mcAsmInfo, *instrInfo, *regInfo);
+  auto *asmPrinter = target->createMCInstPrinter(Triple(triple), 1, *mcAsmInfo, *instrInfo, *regInfo);
+  asmPrinter->printInst(&inst, 0, string(), *subTargetInfo, llvm::outs());
+  llvm::outs() << '\n';
 
-    asmPrinter->printInst(&inst, 0, "", *subTargetInfo, llvm::outs());
-    llvm::outs() << '\n';
-
-    for (auto c : buf)
-    {
-        llvm::outs() << format_hex((unsigned char)c, 4) << ' ';
-    }
-
-    return 0;
+  return 0;
 }
